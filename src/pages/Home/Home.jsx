29ef923feb00e89bbeea825a3c8ee412a98d39da
Musab Al Mahi <musabalmahi53@gmail.com>
2023-10-17 14:22:37 +0600

    my second half project already finished

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,30 @@
+import { useLoaderData } from "react-router-dom";
 import Header from "../shared/Header/Header";
 import LeftSideNav from "../shared/LeftSideNav/LeftSideNav";
 import Navbar from "../shared/Navbar/Navbar";
 import RightSideNav from "../shared/RightSideNav/RightSideNav";
 import BreakingNews from "./BreakingNews";
+import NewsCard from "./NewsCard";
 
 const Home = () => {
+  const news =useLoaderData();
+  console.log(news);
   return (
     <div>
       <Header></Header>
       <BreakingNews></BreakingNews>
       <Navbar></Navbar>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-        <div className="border">
+        <div>
           <LeftSideNav></LeftSideNav>
         </div>
-        <div className="md:col-span-2 border">
-          <h2 className="text-4xl">News Coming Soon..</h2>
+          {/* {news container} */}
+        <div className="md:col-span-2 ">
+       {
+        news.map(aNews=><NewsCard key={aNews._id} news={aNews}></NewsCard>)
+       }
         </div>
-        <div className="border">
+        <div >
           <RightSideNav></RightSideNav>
         </div>
       </div>
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,7 +1,15 @@
 import { Link, NavLink } from "react-router-dom";
 import userDefaultPic from "../../../assets/user.png";
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const Navbar = () => {
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleSignOut = () => {
+    logOut().then().catch();
+  };
+
   const navLinks = (
     <>
       <li>
@@ -12,6 +20,9 @@ const Navbar = () => {
       </li>
       <li>
         <NavLink to="/career">Career</NavLink>
+      </li>
+      <li>
+        <NavLink to="/login">Login</NavLink>
       </li>
     </>
   );
@@ -53,9 +64,15 @@ const Navbar = () => {
             <img src={userDefaultPic} />
           </div>
         </label>
-        <Link>
-          <button className="btn">LogIn</button>
-        </Link>
+        {user ? (
+          <button onClick={handleSignOut} className="btn">
+            Sign Out
+          </button>
+        ) : (
+          <Link to="/login">
+            <button className="btn">LogIn</button>
+          </Link>
+        )}
       </div>
     </div>
   );
